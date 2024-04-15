b710f0117abe83233559d991237f8cf8e65d12af
milicamilojevic01 <mm20200092@student.fon.bg.ac.rs>
2024-04-15 21:51:51 +0200

    omogucena pretraga

--- reactprojekat/src/SpoljniApi/IngredientTable.jsx
+++ reactprojekat/src/SpoljniApi/IngredientTable.jsx
@@ -1,47 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './Ingredients.css';
-import IngredientRow from './IngredientRow';
-const IngredientTable = () => {
-  const [ingredients, setIngredients] = useState([]);
+//dokumentacija za api:
+  //https://fdc.nal.usda.gov/api-guide.html#bkmk-6 
 
-  useEffect(() => {
-    const fetchIngredients = async () => {
-      try {
-        //dokumentacija za api:
-        //https://fdc.nal.usda.gov/api-guide.html#bkmk-6 
-        const response = await axios.get('https://api.nal.usda.gov/fdc/v1/foods/search?query=&pageSize=50&api_key=HltNbgWlYukGRJua7Z8ed0Y2A2V6LuE13PpZ5d9k');
-        const filteredIngredients = response.data.foods.filter(ingredient => ingredient.foodNutrients);
-        setIngredients(filteredIngredients);
-      } catch (error) {
-        console.error('Error fetching ingredients:', error);
-      }
-    }; 
-    fetchIngredients();
-  }, []);
-
-  return (
-    <div className="ingredient-table">
-      <h2 className="ingredient-title">Lista sastojaka</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Naziv</th>
-            <th>Kalorije</th>
-            <th>Proteini</th>
-            <th>Ugljeni hidrati</th>
-            <th>Masti</th>
-          </tr>
-        </thead>
-        <tbody>
-         {ingredients.map(ingredient => (
-            <IngredientRow key={ingredient.fdcId} ingredient={ingredient} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+  import React, { useState, useEffect } from 'react';
+  import axios from 'axios';
+  import './Ingredients.css';
+  import IngredientRow from './IngredientRow';
   
-};
-
-export default IngredientTable;
+  const IngredientTable = () => {
+    const [ingredients, setIngredients] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+  
+    useEffect(() => {
+      const fetchIngredients = async () => {
+        try {
+          const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchTerm}&pageSize=50&api_key=HltNbgWlYukGRJua7Z8ed0Y2A2V6LuE13PpZ5d9k`);
+          const filteredIngredients = response.data.foods.filter(ingredient => ingredient.foodNutrients);
+          setIngredients(filteredIngredients);
+        } catch (error) {
+          console.error('Error fetching ingredients:', error);
+        }
+      }; 
+      fetchIngredients();
+    }, [searchTerm]);
+  
+    const handleSearchChange = (event) => {
+      setSearchTerm(event.target.value);
+    };
+  
+    return (
+      <div className="ingredient-table">
+        <h2 className="ingredient-title">Lista sastojaka</h2>
+        <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Pretraži sastojke..." />
+        <table>
+          <thead>
+            <tr>
+              <th>Naziv</th>
+              <th>Kalorije</th>
+              <th>Proteini</th>
+              <th>Ugljeni hidrati</th>
+              <th>Masti</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ingredients.map(ingredient => (
+              <IngredientRow key={ingredient.fdcId} ingredient={ingredient} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  };
+  
+  export default IngredientTable;
+  
