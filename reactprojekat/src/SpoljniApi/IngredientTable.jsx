8436097a130358502ca816d07eaa183e8c77ac39
milicamilojevic01 <mm20200092@student.fon.bg.ac.rs>
2024-04-15 21:59:31 +0200

    dodata paginacija na tabelu

--- reactprojekat/src/SpoljniApi/IngredientTable.jsx
+++ reactprojekat/src/SpoljniApi/IngredientTable.jsx
@@ -1,89 +1,121 @@
 //dokumentacija za api:
   //https://fdc.nal.usda.gov/api-guide.html#bkmk-6 
   import React, { useState, useEffect } from 'react';
-  import axios from 'axios';
-  import './Ingredients.css';
-  import IngredientRow from './IngredientRow';
-  
-  const IngredientTable = () => {
-    const [ingredients, setIngredients] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const [sortBy, setSortBy] = useState('energy'); // Početni kriterijum sortiranja
-  
-    const getNutrientValue = (ingredient, nutrientName) => {
-      const nutrient = ingredient.foodNutrients.find(nutrient => nutrient.nutrientName === nutrientName);
-      return nutrient ? nutrient.value : 0;
-    };
-  
-    useEffect(() => {
-      const fetchIngredients = async () => {
-        try {
-          const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchTerm}&pageSize=50&api_key=HltNbgWlYukGRJua7Z8ed0Y2A2V6LuE13PpZ5d9k`);
-          const filteredIngredients = response.data.foods.filter(ingredient => ingredient.foodNutrients);
-          setIngredients(filteredIngredients);
-        } catch (error) {
-          console.error('Error fetching ingredients:', error);
-        }
-      }; 
-      fetchIngredients();
-    }, [searchTerm]);
-  
-    const handleSearchChange = (event) => {
-      setSearchTerm(event.target.value);
-    };
-  
-    const handleSortChange = (event) => {
-      setSortBy(event.target.value);
-    };
-  
-    const sortedIngredients = [...ingredients].sort((a, b) => {
-      //  logika za sortiranje po izabranom kriterijumu
-      switch (sortBy) {
-        case 'energy':
-          return getNutrientValue(a, 'Energy') - getNutrientValue(b, 'Energy');
-        case 'protein':
-          return getNutrientValue(a, 'Protein') - getNutrientValue(b, 'Protein');
-        case 'carbs':
-          return getNutrientValue(a, 'Carbohydrate, by difference') - getNutrientValue(b, 'Carbohydrate, by difference');
-        case 'fat':
-          return getNutrientValue(a, 'Total lipid (fat)') - getNutrientValue(b, 'Total lipid (fat)');
-        default:
-          return 0;
-      }
-    });
-  
-    return (
-      <div className="ingredient-table">
-        <h2 className="ingredient-title">Lista sastojaka</h2>
-        <div className="controls">
-          <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Pretraži sastojke..." />
-          <label htmlFor="sort">Sortiraj po:</label>
-          <select id="sort" value={sortBy} onChange={handleSortChange}>
-            <option value="energy">Kalorije</option>
-            <option value="protein">Proteini</option>
-            <option value="carbs">Ugljeni hidrati</option>
-            <option value="fat">Masti</option>
-          </select>
-        </div>
-        <table>
-          <thead>
-            <tr>
-              <th>Naziv</th>
-              <th>Kalorije</th>
-              <th>Proteini</th>
-              <th>Ugljeni hidrati</th>
-              <th>Masti</th>
-            </tr>
-          </thead>
-          <tbody>
-            {sortedIngredients.map(ingredient => (
-              <IngredientRow key={ingredient.fdcId} ingredient={ingredient} />
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
+import axios from 'axios';
+import './Ingredients.css';
+import IngredientRow from './IngredientRow';
+
+const IngredientTable = () => {
+  const [ingredients, setIngredients] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [sortBy, setSortBy] = useState('energy'); // Početni kriterijum sortiranja
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 10;
+
+  const getNutrientValue = (ingredient, nutrientName) => {
+    const nutrient = ingredient.foodNutrients.find(nutrient => nutrient.nutrientName === nutrientName);
+    return nutrient ? nutrient.value : 0;
   };
-  
-  export default IngredientTable;
-  
+
+  useEffect(() => {
+    const fetchIngredients = async () => {
+      try {
+        const response = await axios.get(`https://api.nal.usda.gov/fdc/v1/foods/search?query=${searchTerm}&pageSize=50&api_key=HltNbgWlYukGRJua7Z8ed0Y2A2V6LuE13PpZ5d9k`);
+        const filteredIngredients = response.data.foods.filter(ingredient => ingredient.foodNutrients);
+        setIngredients(filteredIngredients);
+      } catch (error) {
+        console.error('Error fetching ingredients:', error);
+      }
+    }; 
+    fetchIngredients();
+  }, [searchTerm]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+    setCurrentPage(1); // Resetovanje trenutne stranice prilikom promene pretrage
+  };
+
+  const handleSortChange = (event) => {
+    setSortBy(event.target.value);
+  };
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+  const sortedIngredients = [...ingredients].sort((a, b) => {
+    //  logika za sortiranje po izabranom kriterijumu
+    switch (sortBy) {
+      case 'energy':
+        return getNutrientValue(a, 'Energy') - getNutrientValue(b, 'Energy');
+      case 'protein':
+        return getNutrientValue(a, 'Protein') - getNutrientValue(b, 'Protein');
+      case 'carbs':
+        return getNutrientValue(a, 'Carbohydrate, by difference') - getNutrientValue(b, 'Carbohydrate, by difference');
+      case 'fat':
+        return getNutrientValue(a, 'Total lipid (fat)') - getNutrientValue(b, 'Total lipid (fat)');
+      default:
+        return 0;
+    }
+  });
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = sortedIngredients.slice(indexOfFirstItem, indexOfLastItem);
+
+  return (
+    <div className="ingredient-table">
+      <h2 className="ingredient-title">Lista sastojaka</h2>
+      <div className="controls">
+        <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Pretraži sastojke..." />
+        <label htmlFor="sort">Sortiraj po:</label>
+        <select id="sort" value={sortBy} onChange={handleSortChange}>
+          <option value="energy">Kalorije</option>
+          <option value="protein">Proteini</option>
+          <option value="carbs">Ugljeni hidrati</option>
+          <option value="fat">Masti</option>
+        </select>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>Naziv</th>
+            <th>Kalorije</th>
+            <th>Proteini</th>
+            <th>Ugljeni hidrati</th>
+            <th>Masti</th>
+          </tr>
+        </thead>
+        <tbody>
+          {currentItems.map(ingredient => (
+            <IngredientRow key={ingredient.fdcId} ingredient={ingredient} />
+          ))}
+        </tbody>
+      </table>
+      <Pagination
+        itemsPerPage={itemsPerPage}
+        totalItems={sortedIngredients.length}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
+    </div>
+  );
+};
+
+const Pagination = ({ itemsPerPage, totalItems, currentPage, onPageChange }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <ul className="pagination">
+      {pageNumbers.map(number => (
+        <li key={number}>
+          <button onClick={() => onPageChange(number)}>{number}</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default IngredientTable;
